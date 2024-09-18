cc54850bedfb00c81c39c610312a9c102c798d15
manoelsouzadev <manoel.souza280@gmail.com>
2024-09-17 23:57:02 -0300

    Commit inicial

--- app/src/main/java/br/edu/scl/ifsp/sdm/fastcalculation/GameActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/fastcalculation/GameActivity.kt
@@ -8,9 +8,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import br.edu.ifsp.scl.fastcalculation.GameFragment
 import br.edu.ifsp.scl.fastcalculation.OnPlayGame
 import br.edu.ifsp.scl.fastcalculation.WelcomeFragment
+import br.edu.ifsp.sdm.fastcalculation.GameFragment
 import br.edu.scl.ifsp.sdm.fastcalculation.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity(), OnPlayGame  {
